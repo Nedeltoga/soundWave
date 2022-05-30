9f51ffa8ff74588b5aa529289ae2ce2979362168
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-05-30 20:27:40 +0200

    change landing img

--- src/LandpageImg.js
+++ src/LandpageImg.js
@@ -2,7 +2,7 @@ function LandpageImg() {
     return <img
     alt="Black woman with a pleased gesture"
     src={process.env.PUBLIC_URL + '/images/landing-page-girl.png'}
-    width="425"
+    width="480"
     id="landImage"
     />
 }
